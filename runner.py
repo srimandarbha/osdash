487b7e71221b77c9f7a288d3ac64ae9a5c88fd5c
root <root@dnjrhel73.barnesandnoble.com>
2018-09-26 20:08:03 -0400

    edit

--- runner.py
+++ runner.py
@@ -2,9 +2,25 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@127.0.0.1/cindervol'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 Bootstrap(app)
+db = SQLAlchemy(app)
+db.create_all()
+
+class Cindervol(db.Model):
+    __tablename__='cindervol'
+    id = db.Column(db.Integer, primary_key=True)
+    total_cinder_vols = db.Column(db.Integer)
+    available_marked_cinder_vols = db.Column(db.Integer)
+
+db.create_all()
+c = Cindervol(total_cinder_vols=11,available_marked_cinder_vols=5)
+db.session.add(c)
+db.session.commit()
 
 @app.route('/')
 def homepage():
@@ -12,4 +28,4 @@ def homepage():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=80)
